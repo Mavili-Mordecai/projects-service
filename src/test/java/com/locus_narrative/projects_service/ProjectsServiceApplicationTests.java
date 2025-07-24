3d0450e90689debb2f5ba91fcd048a28b3760127
Mavili-Mordecai <mavilimordecai@yandex.ru>
2025-07-24 22:22:38 +0300

    Initial commit

---
+++ src/test/java/com/locus_narrative/projects_service/ProjectsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locus_narrative.projects_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
